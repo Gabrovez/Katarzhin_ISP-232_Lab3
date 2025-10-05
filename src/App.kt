e558188c09d2b0a53bb58902a2e2670938e4564a
Gabrovez <gabrovez321@gmail.com>
2025-10-05 18:55:23 +0300

    Шаг 10. Анонимные функции 1

--- src/App.kt
+++ src/App.kt
@@ -1,10 +1,6 @@
-fun usePotion(name: String, power : Int){
-    println("Использовано $name (+$power HP)")
-}
-fun usePotion(power : Int, name: String ){
-    println("Использовано $name (+$power MP)")
+val greet = fun(name: String){
+    println("Hello, $name!")
 }
 fun main(){
-    usePotion("Лечебное",50)
-    usePotion(30, "Магическое")
+    greet("Player")
 }
