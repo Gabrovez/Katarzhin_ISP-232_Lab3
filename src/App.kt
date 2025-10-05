973ec22ae5ed081c9becd50eb18629ad6dd2e9dc
Gabrovez <gabrovez321@gmail.com>
2025-10-05 18:43:11 +0300

    Шаг 9. Перегрузка функций (Function Overloading) 1

--- src/App.kt
+++ src/App.kt
@@ -1,6 +1,22 @@
-fun isplayeralive (HP: Int): Boolean = HP > 0
+fun sum(a: Int, b: Int): Int{
+        return a + b
+}
+fun sum(a: Double, b: Double): Double{
+        return a + b
+}
+fun sum(a: Int, b: Int, c: Int): Int{
+        return a + b + c
+}
+fun sum(a: Int, b: Double): Double{
+    return a + b
+}
+fun sum(a: Double, b: Int): Double{
+        return a + b
+}
 fun main(){
-    println(isplayeralive(10))
-    println(isplayeralive(0))
-
+    val a = sum(1,  2 )
+    val b = sum(1.5, 2.5 )
+    val c = sum(1,  2 , 3 )
+    val d = sum(2, 1.5 )
+    val e = sum(1.5,  2 )
 }
