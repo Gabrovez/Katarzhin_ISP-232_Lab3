6e5b0ce87088417e4dcd50b97d9886f6ae055d04
Gabrovez <gabrovez321@gmail.com>
2025-10-05 18:28:37 +0300

    Шаг 5. Изменение параметров

--- src/App.kt
+++ src/App.kt
@@ -1,8 +1,9 @@
-fun CreateItem (name: String, price: Int, Magic: Boolean = false){
-    println("Предмет: $name | Цена: $price${if (Magic) " (магический)" else ""}")
+fun upgWeapon (weapon: MutableSet<String>){
+    weapon.add("Улучшение +1")
 }
 fun main(){
-    CreateItem("Sword", 50)
-    CreateItem(price = 200, name = "Potion")
-    CreateItem("Staff", Magic = true, price = 150)
+    val sword = mutableSetOf("Меч")
+    upgWeapon(sword)
+    println(sword)
+
 }
