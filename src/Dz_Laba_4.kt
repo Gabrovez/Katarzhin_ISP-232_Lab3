422528b6bb6e912a244bbbbdd241d20fa7144e70
Gabrovez <gabrovez321@gmail.com>
2025-10-05 21:52:56 +0300

    Задание 8

--- src/Dz_Laba_4.kt
+++ src/Dz_Laba_4.kt
@@ -1,13 +1,14 @@
 fun main(){
-    println(checkInventory("Меч"))
-    println(checkInventory("Кот"))
-    println(checkInventory("Зелье"))
+    Castspell()
+    Castspell("Fireball")
+    Castspell("Fireball", 10)
 }
-fun  checkInventory(item: String): Boolean{
-    val items  = arrayOf("Меч", "Кот")
-    var ans = false
-    if(item in items){
-        ans = true
-    }
-    return  ans
+fun Castspell(){
+    println("Каст случайного заклинания")
+}
+fun Castspell(spell: String){
+    println("Каст заклинания $spell")
+}
+fun Castspell(spell: String, power: Int){
+    println("Каст $spell с силой $power")
 }
