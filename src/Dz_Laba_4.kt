f72bd6164cdc029d4c7f310bcd439693fcb25a49
Gabrovez <gabrovez321@gmail.com>
2025-10-05 21:32:15 +0300

    Задание 4

--- src/Dz_Laba_4.kt
+++ src/Dz_Laba_4.kt
@@ -1,6 +1,6 @@
 fun main(){
-    spawnEnemy("Dismerat")
-    spawnEnemy("Векна", isBoss = true)
+    spawnEnemy("Dismerat", 300)
+    spawnEnemy("Король Тьмы", 500, true)
 }
 fun  spawnEnemy(name: String, health: Int = 100, isBoss: Boolean = false){
     println("Враг: $name Здоровье: $health HP${if (isBoss) " | БОСС!" else ""}")
